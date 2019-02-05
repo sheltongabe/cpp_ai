1ead3c3f8fd57d0e56db23fb22fe3f04c46d2cdd
sheltongabe <sheltongabe0@gmail.com>
2019-02-05 12:37:41 -0500

    Finish FileHandler, and adjust CMakeLists to compile to c++11

--- src/file_handler.cpp
+++ src/file_handler.cpp
@@ -7,6 +7,11 @@
  * @version	0.1
  */
 
+#include <fstream>
+#include <sstream>
+#include <exception>
+#include <iostream>
+
 #include "file_handler.h"
 
 //
@@ -27,14 +32,46 @@ FileHandler::FileHandler(FileHandler& copy) {
 // readFile (const std::string&) -> std::string
 //
 std::string FileHandler::readFile(const std::string& filename) {
+	// Hold the output
+	std::stringstream fileData;
+	fileData.clear();
 
+	// Open and prepare to read the file
+	try {
+		// buffer
+		std::string line;
+		std::ifstream file(filename);
+
+		// Input the file until the end is hit
+		while(!file.eof()) {
+			file >> line;
+			fileData << line;
+		}
+		file.close();
+	}
+	catch(std::exception e) {
+		std::cout << "Failed to read file: " << filename << '\n';
+		throw e;
+	}
+
+	// No error
+	return fileData.str();
 }
 
 // 
 // writeFile (const std::string&, const std::string&) -> void
 //
 void FileHandler::writeFile(const std::string& filename, const std::string& data) {
-
+	// Try to Open the file
+	try {
+		std::ofstream file(filename, std::ofstream::out);
+		file << data;
+		file.close();
+	}
+	catch(std::exception e) {
+		std::cout << "Failed to write to file: " << filename << '\n';
+		throw e;
+	}
 }
 
 // 
