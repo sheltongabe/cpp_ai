4f694cca222a9c6b994d324c5fa8ee904f723adb
sheltongabe <sheltongabe0@gmail.com>
2019-02-12 13:02:41 -0500

    Read in test puzzle and print structure

--- test/src/node_test.cpp
+++ test/src/node_test.cpp
@@ -11,8 +11,22 @@ int main() {
 
 	auto testGraph = GraphFileHandler<int>::readGraph("data.dat");
 
-	//std::cout << "Graph ID: " << testGraph.graphId << '\n';
-	//std::cout << "Node ID: " << testGraph.getNode("Hello Nodes").nodeId << '\n';
+	auto nodeMap = testGraph.nodes;
+	auto edgeMap = testGraph.edges;
+
+	std::cout << "Nodes:\n" << "--------------------\n";
+	for(auto itr = nodeMap.begin(); itr != nodeMap.end(); ++itr) {
+		std::cout << "NodeId: " << itr->second.nodeId << '\n';
+		std::cout << "StateId: " << itr->second.stateId << '\n';
+	}
+
+	std::cout << "Edges:\n" << "--------------------\n";
+	for(auto itr = edgeMap.begin(); itr != edgeMap.end(); ++itr) {
+		std::cout << "EdgeId: " << itr->second.edgeId << '\n';
+		std::cout << "Source: " << itr->second.startNode << '\n';
+		std::cout << "Destination: " << itr->second.endNode << '\n';
+		std::cout << "Cost: " << itr->second.cost << '\n';
+	}
 
 	return 0;
 }
